7dbdc1564c10f36d5a10d1081cf3fdae9e6f0b2f
klssnv <klssneva@icloud.com>
2025-12-19 01:48:47 +0900

    description added

---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+}
+
+// gendiff -h
